4c447a5dfd1df92601ea6bffc6ca5e472eed0ab2
JulienPeyrache <85626373+JulienPeyrache@users.noreply.github.com>
2022-06-20 14:35:50 +0200

    Liste tokens à vérifier

--- src/e_regexp.ml
+++ src/e_regexp.ml
@@ -66,38 +66,53 @@ let keyword_regexp s = str_regexp (char_list_of_string s)
 (* La liste des expressions régulières permettant d'identifier les tokens du langage E *)
 let list_regexp : (regexp * (string -> token option)) list =
   [
-    (keyword_regexp "while",    fun s -> Some (SYM_WHILE));
-    (keyword_regexp "int", fun s -> Some (SYM_INT));
+    (keyword_regexp "while",   fun s -> Some (SYM_WHILE));
+    (keyword_regexp "int",     fun s -> Some (SYM_INT));
     (* begin TODO *)
-    (Eps,       fun s -> Some (SYM_VOID));
-    (Eps,       fun s -> Some (SYM_CHAR));
-    (Eps,       fun s -> Some (SYM_IF));
-    (Eps,       fun s -> Some (SYM_ELSE));
-    (Eps,       fun s -> Some (SYM_RETURN));
-    (Eps,       fun s -> Some (SYM_PRINT));
-    (Eps,       fun s -> Some (SYM_STRUCT));
-    (Eps,       fun s -> Some (SYM_POINT));
-    (Eps,       fun s -> Some (SYM_PLUS));
-    (Eps,       fun s -> Some (SYM_MINUS));
-    (Eps,       fun s -> Some (SYM_ASTERISK));
-    (Eps,       fun s -> Some (SYM_DIV));
-    (Eps,       fun s -> Some (SYM_MOD));
-    (Eps,       fun s -> Some (SYM_LBRACE));
-    (Eps,       fun s -> Some (SYM_RBRACE));
-    (Eps,       fun s -> Some (SYM_LBRACKET));
-    (Eps,       fun s -> Some (SYM_RBRACKET));
-    (Eps,       fun s -> Some (SYM_LPARENTHESIS));
-    (Eps,       fun s -> Some (SYM_RPARENTHESIS));
-    (Eps,       fun s -> Some (SYM_SEMICOLON));
-    (Eps,       fun s -> Some (SYM_COMMA));
-    (Eps,       fun s -> Some (SYM_ASSIGN));
-    (Eps,       fun s -> Some (SYM_EQUALITY));
-    (Eps,       fun s -> Some (SYM_NOTEQ));
-    (Eps,       fun s -> Some (SYM_LT));
-    (Eps,       fun s -> Some (SYM_GT));
-    (Eps,       fun s -> Some (SYM_LEQ));
-    (Eps,       fun s -> Some (SYM_GEQ));
-    (Eps,       fun s -> Some (SYM_IDENTIFIER s));
+    (keyword_regexp "void",    fun s -> Some (SYM_VOID));
+    (keyword_regexp "char",    fun s -> Some (SYM_CHAR));
+    (keyword_regexp "if",      fun s -> Some (SYM_IF));
+    (keyword_regexp "else",    fun s -> Some (SYM_ELSE));
+    (keyword_regexp "return",  fun s -> Some (SYM_RETURN));
+    (keyword_regexp "print",   fun s -> Some (SYM_PRINT));
+    (keyword_regexp "struct",  fun s -> Some (SYM_STRUCT));
+    (char_regexp '.',       fun s -> Some (SYM_POINT));
+    (char_regexp '+',       fun s -> Some (SYM_PLUS));
+    (char_regexp '-',       fun s -> Some (SYM_MINUS));
+    (char_regexp '*',       fun s -> Some (SYM_ASTERISK));
+    (char_regexp '/',       fun s -> Some (SYM_DIV));
+    (char_regexp '%',       fun s -> Some (SYM_MOD));
+    (char_regexp '{',       fun s -> Some (SYM_LBRACE));
+    (char_regexp '}',       fun s -> Some (SYM_RBRACE));
+    (char_regexp '[',       fun s -> Some (SYM_LBRACKET));
+    (char_regexp ']',       fun s -> Some (SYM_RBRACKET));
+    (char_regexp '(',       fun s -> Some (SYM_LPARENTHESIS));
+    (char_regexp ')',       fun s -> Some (SYM_RPARENTHESIS));
+    (char_regexp ';',       fun s -> Some (SYM_SEMICOLON));
+    (char_regexp ',',       fun s -> Some (SYM_COMMA));
+    (char_regexp '=',      fun s -> Some (SYM_ASSIGN));
+    (keyword_regexp "==",       fun s -> Some (SYM_EQUALITY));
+    (keyword_regexp "!=",      fun s -> Some (SYM_NOTEQ));
+    (keyword_regexp "<",       fun s -> Some (SYM_LT));
+    (keyword_regexp ">",       fun s -> Some (SYM_GT));
+    (keyword_regexp "<=",      fun s -> Some (SYM_LEQ));
+    (keyword_regexp ">=",      fun s -> Some (SYM_GEQ));
+    (identifier_material,        fun s -> Some (SYM_IDENTIFIER s));
+    (char_regexp "",       fun s -> Some (SYM_EOF));
+    (char_regexp '!',       fun s -> Some (SYM_BOOL_NOT));
+    (keyword_regexp "&&",       fun s -> Some (SYM_BOOL_AND));
+    (keyword_regexp "||",       fun s -> Some (SYM_BOOL_OR));
+    (keyword_regexp "",       fun s -> Some (SYM_ARROW));
+    (keyword_regexp "|",       fun s -> Some (SYM_BITWISE_OR));
+    (keyword_regexp "&",       fun s -> Some (SYM_BITWISE_AND));
+    (keyword_regexp "~",       fun s -> Some (SYM_BIT_NOT));
+    (char_regexp '&',       fun s -> Some (SYM_AMPERSAND));
+    (keyword_regexp "extern",       fun s -> Some (SYM_EXTERN));
+    (Cat(keyword_regexp "include ", any_string_regexp),       fun s -> Some (SYM_INCLUDE s));
+    (keyword_regexp "alloc",       fun s -> Some (SYM_ALLOC));
+    (any_string_regexp,       fun s -> Some (SYM_STRING s));
+    (any_char_regexp,       fun s -> Some (SYM_CHARACTER s));
+
     (* end TODO *)
     (Cat(keyword_regexp "//",
          Cat(Star (char_range (List.filter (fun c -> c <> '\n') alphabet)),
